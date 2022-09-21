234d883e341ee306554873c2eb68ad9c4218c309
Michel Aycaguer <michel.aycaguer@overactive.com>
2022-09-20 21:16:15 -0300

    Changed folder location for back and front folders

--- updateFiles.sh
+++ updateFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ../Raccoonreco/app.py ./back
-cp ../Raccoonreco/FaceEmbeddings.py ./back
-cp ../Raccoonreco/FaceRecognitionFunctions.py ./back
-cp ../Raccoonreco-frontend/index.html ./front
-cp ../Raccoonreco-frontend/insertUser.html ./front
+cp ../Raccoonreco/app.py ./Dockerfiles/back
+cp ../Raccoonreco/FaceEmbeddings.py ./Dockerfiles/back
+cp ../Raccoonreco/FaceRecognitionFunctions.py ./Dockerfiles/back
+cp ../Raccoonreco-frontend/index.html ./Dockerfiles/front
+cp ../Raccoonreco-frontend/insertUser.html ./Dockerfiles/front
